28be77d0619311e26095a5d6e7456c25b8a04657
Daniel Quach <daniel@shinzu.com>
2014-09-30 09:46:15 -0700

    adding new aws credentials

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
 AWS.config({
-:access_key_id => "AKIAJSUWVNTHE5OYJAHQ",
-:secret_access_key => "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",
+:access_key_id => "AKIAIFB3C2VJIOJMKDXQ",
+:secret_access_key => "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",
 })  
